c8b2a96148b3e4b7d4d20da705c416e5057a4192
zllrunning <zllrunning@gmail.com>
2019-06-22 17:03:56 +0800

    add UPChannelRPN

--- models/heads.py
+++ models/heads.py
@@ -26,6 +26,15 @@ class Corr_Up(nn.Module):
 
 
 #  from pysot : https://github.com/STVIR/pysot
+def xcorr_fast(x, kernel):
+    """group conv2d to calculate cross correlation, fast version
+    """
+    batch = kernel.size()[0]
+    pk = kernel.view(-1, x.size()[1], kernel.size()[2], kernel.size()[3])
+    px = x.view(1, -1, x.size()[2], x.size()[3])
+    po = F.conv2d(px, pk, groups=batch)
+    po = po.view(batch, -1, po.size()[2], po.size()[3])
+    return po
 
 
 def xcorr_depthwise(x, kernel):
@@ -46,6 +55,38 @@ class RPN(nn.Module):
 
     def forward(self, z_f, x_f):
         raise NotImplementedError
+
+
+class UPChannelRPN(RPN):
+    def __init__(self, anchor_num=5, feature_in=256):
+        super(UPChannelRPN, self).__init__()
+
+        cls_output = 2 * anchor_num
+        loc_output = 4 * anchor_num
+
+        self.template_cls_conv = nn.Conv2d(feature_in,
+                feature_in * cls_output, kernel_size=3)
+        self.template_loc_conv = nn.Conv2d(feature_in,
+                feature_in * loc_output, kernel_size=3)
+
+        self.search_cls_conv = nn.Conv2d(feature_in,
+                feature_in, kernel_size=3)
+        self.search_loc_conv = nn.Conv2d(feature_in,
+                feature_in, kernel_size=3)
+
+        self.loc_adjust = nn.Conv2d(loc_output, loc_output, kernel_size=1)
+
+
+    def forward(self, z_f, x_f):
+        cls_kernel = self.template_cls_conv(z_f)
+        loc_kernel = self.template_loc_conv(z_f)
+
+        cls_feature = self.search_cls_conv(x_f)
+        loc_feature = self.search_loc_conv(x_f)
+
+        cls = xcorr_fast(cls_feature, cls_kernel)
+        loc = self.loc_adjust(xcorr_fast(loc_feature, loc_kernel))
+        return cls, loc
 
 
 class DepthwiseXCorr(nn.Module):

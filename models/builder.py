93f19316039d91a791840d5c0ef8364bd12da55b
2flash <27231912+eungbean@users.noreply.github.com>
2019-08-05 17:20:53 +0900

    Update builder.py
    Import Error fixed: '.' was missed.
    root@root:SiameseX.PyTorch# python demo.py -model SiamFC
    Traceback (most recent call last):
      File "demo.py", line 10, in <module>
        from demo_utils.siamvggtracker import SiamVGGTracker
      File "/data/research/SiameseX.PyTorch/demo_utils/siamvggtracker.py", line 12, in <module>
        from demo_utils.siamese import SiameseNet
      File "/data/research/SiameseX.PyTorch/demo_utils/siamese.py", line 16, in <module>
        import models.builder as builder
      File "/data/research/SiameseX.PyTorch/models/builder.py", line 10, in <module>
        from neck import AdjustLayer, AdjustAllLayer
    ImportError: cannot import name 'AdjustLayer'

--- models/builder.py
+++ models/builder.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 from .heads import Corr_Up, MultiRPN, DepthwiseRPN
 from .backbones import AlexNet, Vgg, ResNet22, Incep22, ResNeXt22, ResNet22W, resnet50, resnet34, resnet18
-from neck import AdjustLayer, AdjustAllLayer
+from .neck import AdjustLayer, AdjustAllLayer
 from .utils import load_pretrain
 
 
